e73a7f3c681c09239009804976036da09e502507
ehteshammuhammad <muhammad.ehtesham@visionet.com>
2019-10-16 22:02:16 +0500

    ongoing

--- test.py
+++ test.py
@@ -1,64 +1,62 @@
 import random
 import string
 import time
-from itertools import permutations
 
-numberMap = {}
+
 
 def randomword():
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(5))
 
-def sumOfNumMap(a):
-    return sum([numberMap[i] for i in a])
 
 def memoize(f):
     results = {}
     def helper(a):
-        num = sumOfNumMap(a)
-        if num not in results:
-            results[num] = f(a)
-        return results[num]
+        if str(a) not in results:
+            results[str(a)] = f(a)
+        return results[str(a)]
     return helper
 
 
-
 @memoize
-def task(a):
-    res = []
+def task(a):   
+    res = [] 
     if (len(a) == 1):
         return a
     if (len(a) == 2):
-        return [a[0]] + [a[1]], [a[1]] + [a[0]]
-    for i in range(len(a)):
-        k = [[a[i]] + j for j in task(a[:i] + a[i + 1:])]
-        for l in k:
-            res.append(l)
+        return [a[0]] + [a[1]], [a[1]] + [a[0]]    
+    k = task(a[1:])  
+    for l in k:
+        res.append(l)  
     return res
+
+def finaliser(a):
+    t = task(a)
+    print(t)
+    r = [[a[i]]+[a[i-1] if wd == a[i] else wd for wd in j] for i in range(len(a)) for j in t]
+    return r
+
+finaliser("abcde")
 
 
 
 def preprocess(a):  # For Finding Repeated Items
     checkRepeat = {}
-    repeated = {}    
+    repeated = {}
     for i in range(len(a)):
         if (a[i] in checkRepeat):
             temp = a[i]
             a[i] = randomword()
             repeated[a[i]] = temp
         checkRepeat[a[i]] = 1
-    r = list(checkRepeat)
-    randNums = random.sample(range(1, 100), len(r)) 
-    for j,k in enumerate(r):        
-        numberMap[k] = randNums[j]
-    return r, repeated
+    return list(checkRepeat), repeated
 
 
 def main(a):
     a = list(a)
     final = {}
     x, y = preprocess(a)
-    result = task(x)
+    result = finaliser(x)
     if (len(y) == 0):
         return result
     for i in range(len(result)):
@@ -70,12 +68,12 @@ def main(a):
 
 
 
-testInput = ['1','2','3','4','5','6']       #Example of string with repeated items
+# testInput = "123456789a"
 
-t1 = time.time()
-print(len(main(testInput)))
+# t = time.time()
+# print(len(main(testInput)))
 
-print("Done", time.time() - t1, "seconds")
+# print("Done", time.time() - t, "seconds")
 
 
 
